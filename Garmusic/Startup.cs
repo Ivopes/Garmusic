553c055d48e53600a3c39c60059fe23ecfaeaa6f
Ivo Pesak <hapesak@seznam.cz>
2020-09-23 12:26:12 +0200

    alter rootPath

--- Garmusic/Startup.cs
+++ Garmusic/Startup.cs
@@ -54,7 +54,7 @@ namespace Garmusic
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/dist";
+                configuration.RootPath = "ClientApp/dist/WEB-BAK";
             });
         }
 
