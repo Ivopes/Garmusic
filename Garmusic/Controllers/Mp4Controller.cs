6bd5cfec46d0d79eec1a6a4fa3fbb5eaf7c06bbc
Ivo Pesak <hapesak@seznam.cz>
2020-09-27 13:09:22 +0200

    removed unnecessary using statements

--- Garmusic/Controllers/Mp4Controller.cs
+++ Garmusic/Controllers/Mp4Controller.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
