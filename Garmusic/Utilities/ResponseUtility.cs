7b3f87ab3b22b4df016f39f5e3d9c8b1f0cb631d
Ivo Pesak <hapesak@seznam.cz>
2020-11-07 19:38:50 +0100

    add utility

---
+++ Garmusic/Utilities/ResponseUtility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garmusic.Utilities
+{
+    public static class ResponseUtility
+    {
+        public static async Task<string> BodyStreamToStringAsync(Stream stream, int length)
+        {
+            using var s = stream;
+
+            byte[] buffer = new byte[length];
+
+            await stream.ReadAsync(buffer, 0, length);
+
+            string body = Encoding.UTF8.GetString(buffer);
+
+            return body;
+        }
+    }
+}
