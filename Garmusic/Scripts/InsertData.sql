6f45763c5391aa1d60173ff886334f45011361c6
Ivo Pesak <hapesak@seznam.cz>
2021-04-21 18:18:57 +0200

    Add sql scripts for creating local database

---
+++ Garmusic/Scripts/InsertData.sql
@@ -0,0 +1,10 @@
+USE [musicSystem]
+GO
+SET IDENTITY_INSERT [dbo].[Storage] ON 
+GO
+INSERT [dbo].[Storage] ([StorageID], [Name]) VALUES (1, N'Dropbox')
+GO
+INSERT [dbo].[Storage] ([StorageID], [Name]) VALUES (2, N'Google Drive')
+GO
+SET IDENTITY_INSERT [dbo].[Storage] OFF
+GO
